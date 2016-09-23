51e0f05669932e5a4ea95b0b4d706e906185f140
ksato <ksato@iastate.edu>
2016-09-22 23:40:50 -0500

    Added App.cs trying to figure out Navigation page

---
+++ Login_New/MyFirstApp/App.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace MyFirstApp
+{
+		public class App
+		{
+		public App()
+		{
+			// The root page of your application
+			NavigatioinPage page = new NavigationPage();
+		}
+		}
+}
