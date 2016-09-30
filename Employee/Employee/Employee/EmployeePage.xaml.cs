20e71ad786f4b9c84247ab47abbab29c10ea2331
Zijie Deng <dengzj@iastate.edu>
2016-09-30 12:20:41 -0500

    Add Employee Main Pages

--- Employee/Employee/Employee/EmployeePage.xaml.cs
+++ Employee/Employee/Employee/EmployeePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using System;
+
+using Xamarin.Forms;
 
 namespace Employee
 {
@@ -7,6 +9,28 @@ namespace Employee
 		public EmployeePage()
 		{
 			InitializeComponent();
+		}
+
+		void CreateButtonClicked(object sender, EventArgs args)
+		{
+			Navigation.PushModalAsync(new EmployeeSignupPage());
+		}
+
+		void LoginButtonClicked(object sender, EventArgs args)
+		{
+			Navigation.PushModalAsync(new EmployeeHomePage());
+		}
+
+		void PasswordButtonClicked(object sender, EventArgs args)
+		{
+			Navigation.PushModalAsync(new EmployeePasswordResetPage());
+		}
+
+		async void HelpButtonClicked(object sender, EventArgs args)
+		{
+			await DisplayAlert("Contact Information",
+				"Our Email Address: LiveWell@iastate.edu" +'\n'+"Our Phone Number: 515-555-5555",
+				"Go Back");
 		}
 	}
 }
