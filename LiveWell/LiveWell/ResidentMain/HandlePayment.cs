c7be4deca267df57bd94055a1aad2b494fde26a8
ethwillz <ewwilliams6@gmail.com>
2016-10-05 13:52:15 -0500

    Messing around with httpclient to try and get it to go through

--- LiveWell/LiveWell/ResidentMain/HandlePayment.cs
+++ LiveWell/LiveWell/ResidentMain/HandlePayment.cs
@@ -1,8 +1,10 @@
-﻿using Newtonsoft.Json;
+﻿using ModernHttpClient;
+using Newtonsoft.Json;
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Xamarin.Auth;
 
 namespace LiveWell.ResidentMain
 {
@@ -37,19 +39,18 @@ namespace LiveWell.ResidentMain
         //Runs payment through service using json input and returns success or not
         async Task RunAsyncPayment(PayDetails payDetails)
         {
-            var getToken = new HttpClient();
+            var getToken = new HttpClient(new NativeMessageHandler());
             getToken.BaseAddress = new Uri("https://api.sandbox.paypal.com/v1/oauth2/token");
             getToken.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             getToken.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue("en_US"));
-            getToken.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("EOJ2S", "AZO0N5GJVZDE0Ubt417QqdeG22iEFFhO-uMjhJcSPKK6qUfoQ2yG5LCzw-JnsZ0waDT3o2iAGlUCzvoh:EKLht0duhg4jH78ndGHHyAkjb0yt457GguhTLkMQtJoqLJBf4aqdogGW53hRdbuas8TCrqOxpYSPpVEv");
-            getToken.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded"));
+            getToken.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("AZO0N5GJVZDE0Ubt417QqdeG22iEFFhO-uMjhJcSPKK6qUfoQ2yG5LCzw-JnsZ0waDT3o2iAGlUCzvoh", "EKLht0duhg4jH78ndGHHyAkjb0yt457GguhTLkMQtJoqLJBf4aqdogGW53hRdbuas8TCrqOxpYSPpVEv");
+            getToken.DefaultRequestHeaders.TryAddWithoutValidation("content-type", "application/x-www-form-urlencoded");
 
-            string json = JsonConvert.SerializeObject(new OAuthDetails("clientcredentials"));
-
-            HttpResponseMessage tokenResponse = await getToken.GetAsync(json);
+            OAuthDetails clientCredentials = new OAuthDetails("client_credentials");
+            HttpResponseMessage tokenResponse = await getToken.PostAsJsonAsync("/v1/oauth2/token", clientCredentials);
             AccessResponse accessInfo = await tokenResponse.Content.ReadAsAsync<AccessResponse>();
 
-            var postPayment = new HttpClient();
+            var postPayment = new HttpClient(new NativeMessageHandler());
             postPayment.BaseAddress = new Uri("https://api.sandbox.paypal.com/v1/payments/payment");
             postPayment.DefaultRequestHeaders.Accept.Clear();
             postPayment.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
