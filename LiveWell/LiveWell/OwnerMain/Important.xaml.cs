4110e938b1301784837bd58d6a6b1b5874e3d4ce
Malcolm Johnson <maj401@iastate.edu>
2016-10-10 09:17:23 -0500

    :added skelatal code for important and more

--- LiveWell/LiveWell/OwnerMain/Important.xaml.cs
+++ LiveWell/LiveWell/OwnerMain/Important.xaml.cs
@@ -10,6 +10,26 @@ namespace LiveWell
 		public Important()
 		{
 			InitializeComponent();
+
+			List<Notification> notifications = new List<Notification>()
+			{
+				new Notification("PLACEHOLDER", "TEST"),
+			};
+
+			quickview.ItemsSource = notifications;
+			quickview.RowHeight = 60;
+		}
+
+		public class Notification
+		{
+			public Notification(String summary, String details)
+			{
+				this.Summary = summary;
+				this.Details = details;
+			}
+
+			public String Summary { get; set; }
+			public String Details { get; set; }
 		}
 	}
 }
--- LiveWell/LiveWell/OwnerMain/OwnerMore.xaml.cs
+++ LiveWell/LiveWell/OwnerMain/OwnerMore.xaml.cs
@@ -10,6 +10,31 @@ namespace LiveWell
 		public OwnerMore()
 		{
 			InitializeComponent();
+
+			List<Page> pages = new List<Page>()
+			{
+				new Page("Add Property"),
+				new Page("Maintenance")
+			};
+
+			otherPages.ItemsSource = pages;
+			otherPages.RowHeight = 60;
+		}
+
+		public void onTap(object sender, EventArgs e)
+		{
+			((ListView)sender).SelectedItem = null;
+			Navigation.PushModalAsync(new Maintenance());
+		}
+
+		public class Page
+		{
+			public Page(String page)
+			{
+				this.PageName = page;
+			}
+
+			public string PageName { get; set; }
 		}
 	}
 }
