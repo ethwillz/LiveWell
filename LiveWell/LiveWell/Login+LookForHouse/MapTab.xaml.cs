9385b2d653d2f5b91fd20d910e28cc59746b7340
ksato <ksato@iastate.edu>
2016-10-14 15:49:19 -0500

    LocationService works :)

--- LiveWell/LiveWell/Login+LookForHouse/MapTab.xaml.cs
+++ LiveWell/LiveWell/Login+LookForHouse/MapTab.xaml.cs
@@ -5,30 +5,48 @@ using System.Net.Http;
 using Xamarin.Forms;
 using static LiveWell.ConnectHelpers;
 using Xamarin.Forms.Maps;
+using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
 
 
 namespace LiveWell
 {
+
 	public partial class MapTab : ContentPage
 	{
 		public MapTab()
 		{
 			InitializeComponent();
 			populateList();
+			setLocation();
+		}
+
+		async void setLocation()
+		{
+			var locator = CrossGeolocator.Current;
+			var position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+
+			MyMap.MoveToRegion(
+			MapSpan.FromCenterAndRadius(
+					new Xamarin.Forms.Maps.Position(position.Latitude,position.Longitude), Distance.FromMiles(1)));
 		}
 
 		async void populateList()
 		{
 			DatabaseConnect conn = new DatabaseConnect();
-			List<Address> addresses = await conn.getAddress(2);
+			List<Address> addresses = await conn.getAddress(1);
 
 			List<QuickViewAddress> address = new List<QuickViewAddress>();
-
-			address.Add(new QuickViewAddress(addresses[0].address, "TEST: Data from database"));
-
+			for (int i = 0; i < addresses.Count; i++)
+			{
+				address.Add(new QuickViewAddress(addresses[i].address, "TEST: Data from database"));
+			}
 			quickview.ItemsSource = address;
 			quickview.RowHeight = 60;
+
 		}
+
+
 	}
 
 	public class QuickViewAddress
