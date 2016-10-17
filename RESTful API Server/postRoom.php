c8c426201ab045f5465b728df7efc865e4ce6750
ethwillz <ewwilliams6@gmail.com>
2016-10-17 09:06:11 -0500

    Added new files

---
+++ RESTful API Server/postRoom.php
@@ -0,0 +1,29 @@
+<?php
+	header("Content-Type", "application/json");
+
+	//Set up for connection to database
+	$db = new mysqli('mysql.cs.iastate.edu', 'dbu309la04', 'YTGUxudv7py', 'db309la04');
+	
+	//Attempts to connect to database which returns an error if unsuccessful
+	if($db->connect_errno > 0 ){
+		die('Unable to connect to database [' . $db->connect_error . ']');
+	}
+
+	if(isset($_POST)){
+		$input = json_decode(file_get_contents('php://input'), true);
+		
+		$roomID = $input['roomID'];
+		$number = $input['number'];
+		$buildingID = $input['buildingID'];
+		
+		//Sets value of $result to SQL query and returns an error otherwise
+		if(!$result = $db->query("INSERT INTO tblRoom (roomID, number, buildingID) VALUES($roomID, $number, $buildingID)")){
+			die('There was an error running the query [' . $db->error . ']');
+		}
+		
+		//Closes the SQL connection
+		$result->close();
+		$db->close();
+	}
+
+?>
