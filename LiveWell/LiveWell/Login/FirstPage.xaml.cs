2b116b07ded69491c6f3fac7420f957b6473af21
ksato <ksato@iastate.edu>
2016-11-24 22:43:38 -0600

    UI is improved a little bit

--- LiveWell/LiveWell/Login/FirstPage.xaml.cs
+++ LiveWell/LiveWell/Login/FirstPage.xaml.cs
@@ -10,24 +10,24 @@ namespace LiveWell
 		public FirstPage()
 		{
 			String loggedInAs = LiveWell.Helpers.Settings.GeneralSettings;
-			if (loggedInAs != "")
-			{
-				CurrentUser.type = loggedInAs[0];
-				CurrentUser.ID = Convert.ToInt32(loggedInAs.Substring(1));
-			}
+			//if (loggedInAs != "")
+			//{
+			//	CurrentUser.type = loggedInAs[0];
+			//	CurrentUser.ID = Convert.ToInt32(loggedInAs.Substring(1));
+			//}
 
-            if(CurrentUser.type == 'R')
-            {
-				Navigation.PushModalAsync(new MainOrSearchHouse());
-            }
-            if (CurrentUser.type == 'E')
-            {
-                Navigation.PushModalAsync(new EmployeeMain());
-            }
-            if (CurrentUser.type == 'O')
-            {
-                Navigation.PushModalAsync(new Owner());
-            }
+   //         if(CurrentUser.type == 'R')
+   //         {
+			//	Navigation.PushModalAsync(new MainOrSearchHouse());
+   //         }
+   //         if (CurrentUser.type == 'E')
+   //         {
+   //             Navigation.PushModalAsync(new EmployeeMain());
+   //         }
+   //         if (CurrentUser.type == 'O')
+   //         {
+   //             Navigation.PushModalAsync(new Owner());
+   //         }
             InitializeComponent();
 
             logo.Source = ImageSource.FromResource("LiveWell.LiveWellFullLogo.png");
