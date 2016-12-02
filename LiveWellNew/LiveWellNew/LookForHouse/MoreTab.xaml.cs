26c6aafc490b09597e3c3d00531494063376cc75
ksato <ksato@iastate.edu>
2016-12-02 11:41:09 -0600

    moreTab is added. iOS and Android are working fine

---
+++ LiveWellNew/LiveWellNew/LookForHouse/MoreTab.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace LiveWellNew
+{
+	public partial class MoreTab : ContentPage
+	{
+		public MoreTab()
+		{
+			InitializeComponent();
+		}
+
+		public void OnMainButtonClicked(object sender, EventArgs args)
+		{
+			//Navigation.PushModalAsync(new Resident());
+		}
+		public void OnLogOutButtonClicked(object sender, EventArgs args)
+		{
+			//LiveWell.Helpers.Settings.GeneralSettings = "";
+			CurrentUser.type = 'N';
+			Navigation.PushModalAsync(new FirstPage());
+		}
+	}
+}
