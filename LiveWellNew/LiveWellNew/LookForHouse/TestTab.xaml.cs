90a24b8a8ea996d23553c931590b21eb333057ce
ksato <ksato@iastate.edu>
2016-12-03 15:02:53 -0600

    heart and cross images are added on home and favorite tab. iOS and Android are working fine

---
+++ LiveWellNew/LiveWellNew/LookForHouse/TestTab.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace LiveWellNew
+{
+	public partial class TestTab : ContentPage
+	{
+		public TestTab()
+		{
+			InitializeComponent();
+		}
+	}
+}
