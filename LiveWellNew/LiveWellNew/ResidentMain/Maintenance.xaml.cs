9fa5134204884e8e2f3369d820ebce482e10601e
ksato <ksato@iastate.edu>
2016-12-04 21:08:10 -0600

    Maintenance and More are added. iOS and Android are working fine

---
+++ LiveWellNew/LiveWellNew/ResidentMain/Maintenance.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace LiveWellNew
+{
+	public partial class Maintenance : ContentPage
+	{
+		public Maintenance()
+		{
+			InitializeComponent();
+		}
+
+		async void submitClick(object sender, EventArgs e)
+		{
+			DatabasePOST conn = new DatabasePOST();
+			await conn.postMaintenance(CurrentUser.ID, summary.Text, explanation.Text);
+			summary.Text = "";
+			explanation.Text = "";
+			await DisplayAlert("Success", "Maintenance request submitted", "OK");
+		}
+	}
+}
